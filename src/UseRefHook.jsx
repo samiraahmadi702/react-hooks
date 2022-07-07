2e18debeadcf8da90e19ee5b2987fba915661cf8
samira <samiraahmadi702@gmail.com>
2022-07-07 15:59:14 +0430

    + Show previous name input + Show render count

--- src/UseRefHook.jsx
+++ src/UseRefHook.jsx
@@ -1,15 +1,49 @@
+import {useRef, useEffect, useState} from "react";
+
 const UseRefHook = () => {
+
+    const inputRef = useRef(null);
+    const [name, setName] = useState("");
+    const preName = useRef("");
+    const renderCount = useRef(1);
+
+    useEffect(() => {
+        renderCount.current = renderCount.current + 1;
+        // inputRef.current.placeholder = " please enter your name";
+        // inputRef.current.className = "bg-warning";
+    })
+
+    useEffect(() => {
+        preName.current = name;
+    }, [name])
+
+
+    const focusInput = () => {
+        inputRef.current.focus();
+    }
+
     return (
         <>
             <div className="d-grid w-50 gap-2 m-auto">
                 <h5 className="alert alert-success text-center">
-                    UseRef Hook
+                    useRef Hook
                 </h5>
+                <p>{`your name is ${name}`}</p>
+                <p>{`your last name is ${preName.current}`}</p>
+                <p>{`render count is ${renderCount.current}`}</p>
                 <input type="text"
+                       ref={inputRef}
+                       value={name}
+                       onChange={e => {
+                           setName(e.target.value)
+                       }
+                       }
                        className="form-control"
                        placeholder="Enter a name"/>
-                <button className="btn btn-primary btn-block mt-2">
-                    submit khjwqrhka
+                <button
+                    onClick={focusInput}
+                    className="btn btn-primary btn-block mt-2">
+                    submit
                 </button>
             </div>
 
