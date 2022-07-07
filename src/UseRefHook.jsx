d7a63b6a8ffbc93997ef1ea3af84d2d3ed51fbcd
samira <samiraahmadi702@gmail.com>
2022-07-07 13:16:04 +0430

    + Add UseRefHook.jsx

---
+++ src/UseRefHook.jsx
@@ -0,0 +1,19 @@
+const UseRefHook = () => {
+    return (
+        <>
+            <div className="d-grid w-50 gap-2 m-auto">
+                <h5 className="alert alert-success text-center">
+                    UseRef Hook
+                </h5>
+                <input type="text"
+                       className="form-control"
+                       placeholder="Enter a name"/>
+                <button className="btn btn-primary btn-block mt-2">
+                    submit khjwqrhka
+                </button>
+            </div>
+
+        </>
+    )
+}
+export default UseRefHook;
